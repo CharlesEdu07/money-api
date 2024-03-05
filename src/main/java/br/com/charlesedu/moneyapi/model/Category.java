f4b38d49108f8c024fb79b7c8978ae7919e4056a
unknown <charleseduardofaria@gmail.com>
2024-03-05 11:33:06 -0300

    Validando size dos atributos passados para cadastro

--- src/main/java/br/com/charlesedu/moneyapi/model/Category.java
+++ src/main/java/br/com/charlesedu/moneyapi/model/Category.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "category")
@@ -16,6 +17,7 @@ public class Category {
     private long id;
 
     @NotNull
+    @Size(min = 3, max = 30)
     private String categoryName;
 
     public Category() {
