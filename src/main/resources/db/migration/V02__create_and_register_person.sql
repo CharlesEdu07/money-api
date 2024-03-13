d2a4b243450cd03f507f70523354fa2ee4cfe3ed
unknown <charleseduardofaria@gmail.com>
2024-03-13 11:56:01 -0300

    Alterando migrations para produção

--- src/main/resources/db/migration/V02__create_and_register_person.sql
+++ src/main/resources/db/migration/V02__create_and_register_person.sql
@@ -3,12 +3,3 @@ CREATE TABLE person (
     person_name VARCHAR(255) NOT NULL,
     active BOOLEAN NOT NULL DEFAULT TRUE
 );
-
-INSERT INTO
-    person (person_name, active)
-VALUES
-    ('João', TRUE),
-    ('Maria', TRUE),
-    ('José', TRUE),
-    ('Ana', TRUE),
-    ('Carlos', TRUE);
--- src/main/resources/db/migration/V05__create_and_register_postings.sql
+++ src/main/resources/db/migration/V05__create_and_register_postings.sql
@@ -12,7 +12,6 @@ CREATE TABLE posting (
     FOREIGN KEY (person_id) REFERENCES person(id)
 );
 
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Lazer" para a pessoa "John Doe"
 INSERT INTO
     posting (
         posting_description,
@@ -24,190 +23,42 @@ INSERT INTO
     )
 VALUES
     (
-        'Ingresso para o parque de diversões',
+        'Electricity bill',
+        '2024-03-20',
+        100.00,
+        'expense',
+        1,
+        1
+    ),
+    (
+        'Groceries',
+        '2024-03-15',
+        200.00,
+        'expense',
+        3,
+        2
+    ),
+    (
+        'Medicine',
         '2024-03-10',
         50.00,
         'expense',
-        1,
-        11
-    );
-
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Alimentação" para a pessoa "Jane Smith"
-INSERT INTO
-    posting (
-        posting_description,
-        due_date,
-        posting_value,
-        posting_type,
-        category_id,
-        person_id
-    )
-VALUES
+        4,
+        3
+    ),
     (
-        'Compras no mercado',
-        '2024-03-08',
-        100.00,
-        'expense',
-        2,
-        12
-    );
-
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Investimento" para a pessoa "Alice Johnson"
-INSERT INTO
-    posting (
-        posting_description,
-        due_date,
-        posting_value,
-        posting_type,
-        category_id,
-        person_id
-    )
-VALUES
-    (
-        'Compra de ações',
-        '2024-03-15',
-        500.00,
-        'investment',
-        10,
-        13
-    );
-
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Impostos" para a pessoa "Michael Brown"
-INSERT INTO
-    posting (
-        posting_description,
-        due_date,
-        posting_value,
-        posting_type,
-        category_id,
-        person_id
-    )
-VALUES
-    (
-        'Pagamento de IPTU',
-        '2024-03-31',
-        200.00,
-        'expense',
-        11,
-        14
-    );
-
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Outros" para a pessoa "Emily Davis"
-INSERT INTO
-    posting (
-        posting_description,
-        due_date,
-        posting_value,
-        posting_type,
-        category_id,
-        person_id
-    )
-VALUES
-    (
-        'Presente de aniversário',
-        '2024-03-20',
-        30.00,
+        'Rent',
+        '2024-03-01',
+        1500.00,
         'expense',
         5,
-        15
-    );
-
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Farmácia" para a pessoa "Hally"
-INSERT INTO
-    posting (
-        posting_description,
-        due_date,
-        posting_value,
-        posting_type,
-        category_id,
-        person_id
-    )
-VALUES
+        4
+    ),
     (
-        'Medicamentos',
-        '2024-03-12',
-        80.00,
-        'expense',
-        4,
-        16
-    );
-
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Conta de Internet" para a pessoa "Mário"
-INSERT INTO
-    posting (
-        posting_description,
-        due_date,
-        posting_value,
-        posting_type,
-        category_id,
-        person_id
-    )
-VALUES
-    (
-        'Pagamento da conta de internet',
-        '2024-03-15',
-        50.00,
-        'expense',
-        14,
-        17
-    );
-
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Financiamento" para a pessoa "Jorge"
-INSERT INTO
-    posting (
-        posting_description,
-        due_date,
-        posting_value,
-        posting_type,
-        category_id,
-        person_id
-    )
-VALUES
-    (
-        'Parcela do financiamento do carro',
-        '2024-03-20',
-        300.00,
-        'expense',
-        7,
-        24
-    );
-
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Empréstimo" para a pessoa "João Vitor"
-INSERT INTO
-    posting (
-        posting_description,
-        due_date,
-        posting_value,
-        posting_type,
-        category_id,
-        person_id
-    )
-VALUES
-    (
-        'Pagamento da parcela do empréstimo',
+        'Salary',
         '2024-03-25',
-        150.00,
-        'expense',
-        8,
-        18
-    );
-
--- Exemplo de insert para um lançamento financeiro relacionado à categoria "Entregas de aplicativo" para a pessoa "Agnaldo"
-INSERT INTO
-    posting (
-        posting_description,
-        due_date,
-        posting_value,
-        posting_type,
-        category_id,
-        person_id
-    )
-VALUES
-    (
-        'Taxa de entrega de comida',
-        '2024-03-18',
-        20.00,
-        'expense',
-        13,
-        19
+        3000.00,
+        'income',
+        2,
+        5
     );
