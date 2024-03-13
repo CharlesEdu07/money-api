bd2ab3d440f53f80b5b08dc3cf535b287caea8b0
unknown <charleseduardofaria@gmail.com>
2024-03-13 09:39:25 -0300

    Anotando UserAccount como repository

--- src/main/java/br/com/charlesedu/moneyapi/repository/UserAccountRepository.java
+++ src/main/java/br/com/charlesedu/moneyapi/repository/UserAccountRepository.java
@@ -3,9 +3,11 @@ package br.com.charlesedu.moneyapi.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.charlesedu.moneyapi.model.UserAccount;
 
+@Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     public Optional<UserAccount> findByEmail(String email);
 }
